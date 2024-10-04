4cfce1885cbf16a8b8d3f52e4116257cf7ba84ef
haitan2408 <haitan28102408@gmail.com>
2024-10-04 21:18:20 +0700

    up demo method

---
+++ src/ss3_method/Demo.java
@@ -0,0 +1,27 @@
+package ss3_method;
+
+import java.util.Scanner;
+
+public class Demo {
+    String name;
+    int age;
+    static String className =  "C0724M1";
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Demo demo = new Demo();
+        int num = Integer.parseInt(sc.nextLine());
+        if (demo.isPrime(num)) {
+            System.out.println("Prime number");
+        }else {
+            System.out.println("Not Prime number");
+        }
+    }
+}
