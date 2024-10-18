d659f72f0df17d9f6a287cffaedf21994db8c587
haitan2408 <haitan28102408@gmail.com>
2024-10-18 21:16:21 +0700

    up demo TDD

---
+++ src/ss9_tdd/DemoSum.java
@@ -0,0 +1,18 @@
+package ss9_tdd;
+
+import java.util.Scanner;
+
+public class DemoSum {
+
+    public static long sum(int a, int b) {
+        return (long) a + (long) b;
+    }
+
+    public int input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/ss9_tdd/DemoSumTest.java
@@ -0,0 +1,25 @@
+package ss9_tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DemoSumTest {
+
+    @Test
+    void sum() {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+        long result = DemoSum.sum(a, b);
+        assertEquals(4294967294l, result);
+    }
+
+//    input:
+//    output:
+    @Test
+    void sum_2() {
+        int a = Integer.valueOf("a");
+        int b = Integer.MAX_VALUE;
+        long result = DemoSum.sum(a, b);
+        assertEquals(4294967294l, result);
+    }
+}
