d6d58066d399bfa579c235ba4290714393a1eea7
haitan2408 <haitan28102408@gmail.com>
2024-11-04 21:23:21 +0700

    up demo IO FIle

--- src/ss8_clean_code/repository/StudentRepository.java
+++ src/ss8_clean_code/repository/StudentRepository.java
@@ -2,25 +2,83 @@ package ss8_clean_code.repository;
 
 import ss8_clean_code.entity.Student;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRepository {
-//    Generic
-    private static List<Student> students = new ArrayList<>();
+    public static final String SRC_STUDENT = "src/ss8_clean_code/data/student.csv";
 
-    static {
-        students.add(new Student(1, "Nguyễn Văn A", "Hà Nội", 10, "C0724G1"));
-        students.add(new Student(2, "Nguyễn Văn B", "Hà Nội", 10, "C0724G1"));
-        students.add(new Student(3, "Nguyễn Văn C", "Hà Nội", 10, "C0724G1"));
-        students.add(new Student(4, "Nguyễn Văn D", "Hà Nội", 10, "C0724G1"));
-    }
 
     public List<Student> getAll() {
+        List<Student> students = new ArrayList<>();
+        File file = new File(SRC_STUDENT);
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            Student student;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] temp = line.split(",");
+                student = new Student(Integer.parseInt(temp[0]), temp[1], temp[2], Double.parseDouble(temp[3]), temp[4]);
+                students.add(student);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Không tìm thấy file");
+        } catch (IOException e) {
+            System.out.println("Lỗi đọc file");
+        }
         return students;
     }
 
     public void save(Student s) {
-        students.add(s);
+        File file = new File(SRC_STUDENT);
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(s.getCode() + "," + s.getName() + "," + s.getAddress() + "," + s.getPoint() + "," + s.getClassName());
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Lỗi ghi file");
+        }
+    }
+
+    private void writeFile(List<Student> students, boolean append) {
+        File file = new File(SRC_STUDENT);
+
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            fileWriter = new FileWriter(file, append);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            for(Student temp: students) {
+                bufferedWriter.write(toString(temp));
+                bufferedWriter.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Lỗi ghi file");
+        } finally {
+            if(bufferedWriter !=null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    System.out.println("Lỗi đóng file");
+                }
+            }
+        }
+    }
+    private String toString(Student s) {
+        return s.getCode() + "," + s.getName() + "," + s.getAddress() + "," + s.getPoint() + "," + s.getClassName();
     }
 }
