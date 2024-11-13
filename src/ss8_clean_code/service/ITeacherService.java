1ac7b728e55ba35262b03d4608c71cee4f430696
haitan2408 <haitan28102408@gmail.com>
2024-11-13 21:04:56 +0700

    up demo solid

--- src/ss8_clean_code/service/ITeacherService.java
+++ src/ss8_clean_code/service/ITeacherService.java
@@ -1,9 +1,7 @@
 package ss8_clean_code.service;
 
-import ss8_clean_code.entity.Student;
 import ss8_clean_code.entity.Teacher;
 
-import java.util.List;
 
 public interface ITeacherService extends IService<Teacher> {
 
